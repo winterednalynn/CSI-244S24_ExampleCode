c86b9a82296d5a8d18539bb072bd2fe6741125f8
EmeryAtRTC <102991550+EmeryAtRTC@users.noreply.github.com>
2024-04-17 16:10:15 -0700

    Updated Weekn 3

---
+++ Week3/server.js
@@ -0,0 +1,64 @@
+//import express
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+
+//middleware - code that fires on each request
+app.use(express.json());
+//express.urleconded() was deprecated
+app.use(express.urlencoded());
+
+//create your endpoints
+app.get("/", (req, res) => {
+  //Content-Type header that just tells the browser what to expres
+  res.contentType("text/html");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+//endpoint that takes get request sends registration form
+app.get("/registration-form", (req, res) => {
+  res.contentType("text/html");
+  res.sendFile(path.join(__dirname, "public", "registration-form.html"));
+});
+
+app.get("/register", (req, res) => {
+  //information that comes in via a get request is location in query
+  console.log(req.query);
+  res.send(req.query);
+});
+
+app.post("/register", (req, res) => {
+  //information that comes in via a post request is located in the body
+  console.log(req.body);
+  //lets save the data to a file named registrations.json
+  const filePath = path.join(__dirname, "registrations.json");
+  if (fs.existsSync(filePath)) {
+    //read the existing data from the file
+    const registrations = JSON.parse(fs.readFileSync(filePath));
+    //add the new registration to the json file
+    registrations.push(req.body);
+    //update the registrations
+    fs.writeFileSync(filePath, JSON.stringify(registrations));
+  } else {
+    //create a new array with the object that came into the body
+    const registrations = [req.body];
+    //write the registrations
+    fs.writeFileSync(filePath, JSON.stringify(registrations));
+  }
+  res.sendFile(path.join(__dirname, "public", "registrations.html"));
+});
+
+app.get("/view-registrations", (req, res) => {
+  res.contentType("text/html");
+  res.sendFile(path.join(__dirname, "public", "registrations.html"));
+});
+
+app.get("/registrations", (req, res) => {
+  res.contentType("application/json");
+  res.sendFile(path.join(__dirname, "registrations.json"));
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
